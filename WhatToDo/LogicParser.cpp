e5164504d8e04a7f9b7dfc01eb4cd35d8a00ed52
Shu Chang <shuchang.91@gmail.com>
2014-10-04 18:20:31 +0800

    Parser completed

--- WhatToDo/LogicParser.cpp
+++ WhatToDo/LogicParser.cpp
@@ -3,14 +3,6 @@
 LogicParser::LogicParser(void) {
 }
 
-void LogicParser::setUserInput(std::string userInput) {
-	_userInput = userInput;
-}
-
-std::string LogicParser::getUserInput(void) {
-	return _userInput;
-}
-
 Command* LogicParser::getCommandFromUserInput(std::string userInput) {
 	LogicParser::setUserInput(userInput);
 
@@ -19,32 +11,31 @@ Command* LogicParser::getCommandFromUserInput(std::string userInput) {
 	return command;
 }
 
+void LogicParser::setUserInput(std::string userInput) {
+	_userInput = userInput;
+}
+
 Command* LogicParser::createCommand() {
 	string userCommand = LogicParser::getUserCommand();
 	Command* command = NULL;
 
-	if(userCommand == "/delete") {
+	if(userCommand == COMMAND_CLEAR) {
+		command = LogicParser::createClearCommand();
+	} else if(userCommand == COMMAND_DELETE) {
 		command = LogicParser::createDeleteCommand();	
-	}
-	else if(userCommand == "/display") {
-		command = LogicParser::createDisplayCommand();
-	}
-	else if(userCommand == "/edit") {
+	} else if(userCommand == COMMAND_DONE) {
+		command = LogicParser::createDoneCommand();
+	} else if(userCommand == COMMAND_EDIT) {
 		command = LogicParser::createEditCommand();
-	}
-	else if(userCommand == "/load") {
+	} else if(userCommand == COMMAND_LOAD) {
 		command = LogicParser::createLoadCommand();
-	}
-	else if(userCommand == "/redo") {
+	} else if(userCommand == COMMAND_REDO) {
 		command = LogicParser::createRedoCommand();
-	}
-	else if(userCommand == "/search") {
+	} else if(userCommand == COMMAND_SEARCH) {
 		command = LogicParser::createSearchCommand();
-	}
-	else if(userCommand == "/undo") {
+	} else if(userCommand == COMMAND_UNDO) {
 		command = LogicParser::createUndoCommand();
-	}
-	else {
+	} else {
 		command = LogicParser::createAddCommand();
 	}
 
@@ -60,13 +51,50 @@ Command* LogicParser::createAddCommand() {
 	addCommand->setParsedStatus(LogicParser::isValidCommandWithStringParameters());
 
 	if(addCommand->getParsedStatus()) {
-		LogicParser::createAddCommandDetails(addCommand);
+		LogicParser::addTaskDetails(addCommand);
 	}
 
 	return addCommand;
 }
 
-void LogicParser::createAddCommandDetails(Command* addCommand) {
+Command* LogicParser::createEditCommand() {
+	Command* editCommand = new CommandEdit;
+	editCommand->setParsedStatus(LogicParser::isValidCommandWithStringParameters());
+
+	if(editCommand->getParsedStatus()) {
+		LogicParser::addTaskIndex(editCommand);
+		LogicParser::removeEditIndex();
+		LogicParser::addTaskDetails(editCommand);
+	}
+
+	return editCommand;
+}
+
+Command* LogicParser::createSearchCommand() {
+	Command* searchCommand = new CommandSearch;
+	searchCommand->setParsedStatus(LogicParser::isValidCommandWithStringParameters());
+
+	if(searchCommand->getParsedStatus()) {
+		std::string searchString = LogicParser::getParameters(_userInput);
+		searchCommand->setSearchKeyword(searchString);
+	}
+
+	return searchCommand;
+}
+
+void LogicParser::addTaskIndex(Command* command) {
+	int taskIndex = 0;
+	std::istringstream iss(_userInput);
+
+	iss >> taskIndex;
+	command->setTaskIndex(taskIndex);
+}
+
+void LogicParser::removeEditIndex() {
+	_userInput = LogicParser::removeFirstWord(_userInput);
+}
+
+void LogicParser::addTaskDetails(Command* command) {
 	std::string taskParameters = LogicParser::getParameters(_userInput);
 	Task* task = new Task;
 
@@ -75,14 +103,17 @@ void LogicParser::createAddCommandDetails(Command* addCommand) {
 	LogicParser::addTaskName(task, taskParameters);
 }
 
-void LogicParser::addTaskTags(Task* task, std::string taskParameters) {
+void LogicParser::addTaskTags(Task* task, std::string& taskParameters) {
 	std::vector<std::string> taskTags;
 	std::istringstream iss(taskParameters);
-	std::string currentToken;
+	std::string currentWord;
+	taskParameters = "";
 
-	while(iss >> currentToken) {
-		if(LogicParser::isTag(currentToken)) {
-			taskTags.push_back(currentToken);
+	while(iss >> currentWord) {
+		if(LogicParser::isTag(currentWord)) {
+			taskTags.push_back(currentWord);
+		} else {
+			taskParameters += currentWord + " ";
 		}
 	}
 
@@ -90,12 +121,84 @@ void LogicParser::addTaskTags(Task* task, std::string taskParameters) {
 }
 
 bool LogicParser::isTag(std::string word) {
-	return (word.find_first_of("@#") == 0);
+	return (word.find_first_of(IDENTIFIER_TAG) == 0);
 }
 
-void LogicParser::addTaskDateTime(Task* task, std::string taskParameters) {
+void LogicParser::addTaskDateTime(Task* task, std::string& taskParameters) {
+	std::istringstream iss(taskParameters);
+	std::string currentWord;
+	boost::posix_time::ptime currentTime(second_clock::local_time());
+	boost::posix_time::ptime startDateTime;
+	boost::posix_time::ptime endDateTime;
+	boost::posix_time::ptime deadline;
+
+
+	taskParameters = "";
+
+	while(iss >> currentWord) {
+		if(LogicParser::isDateTimeIdentifier(currentWord)) {
+			switch (LogicParser::determineDateTimeType(currentWord)) {
+			case AT:
+			case FROM:
+			case ON:
+				for(size_t i = 0; i < formats_n; i++) {
+					iss.imbue(formats[i]);
+					iss >> startDateTime;
+					if(startDateTime != boost::posix_time::ptime()) {
+						break;
+				    }
+				}
+			case TO:
+				for(size_t i = 0; i < formats_n; i++) {
+					iss.imbue(formats[i]);
+					iss >> endDateTime;
+					if(endDateTime != boost::posix_time::ptime()) {
+						break;
+				    }
+				}
+			case BY:
+				for(size_t i = 0; i < formats_n; i++) {
+					iss.imbue(formats[i]);
+					iss >> deadline;
+					if(deadline != boost::posix_time::ptime()) {
+						break;
+				    }
+				}
+			case INVALID:
+				taskParameters += currentWord + " ";
+			}
+		} else {
+			taskParameters += currentWord + " ";
+		}
+	}
+
 }
+
+bool LogicParser::isDateTimeIdentifier(std::string word) {
+	return (word.find_first_of(IDENTIFIER_DATETIME) == 0);
+
+}
+
+LogicParser::DateTimeType LogicParser::determineDateTimeType(std::string word) {
+	word = LogicParser::transformToLowercase(word);
+
+	if (word == IDENTIFIER_DATETIME_AT) {
+		return LogicParser::DateTimeType::AT;
+	} else if (word == IDENTIFIER_DATETIME_BY) {
+		return LogicParser::DateTimeType::BY;
+	} else if (word == IDENTIFIER_DATETIME_ON) {
+		return LogicParser::DateTimeType::ON;
+	} else if (word == IDENTIFIER_DATETIME_FROM) {
+		return LogicParser::DateTimeType::FROM;
+	} else if (word == IDENTIFIER_DATETIME_TO) {
+		return LogicParser::DateTimeType::TO;
+	} else {
+		return LogicParser::DateTimeType::INVALID;
+	}
+}
+
 void LogicParser::addTaskName(Task* task, std::string taskParameters) {
+	task->setTaskName(LogicParser::trimWhiteSpace(taskParameters));
 }
 
 Command* LogicParser::createDeleteCommand() {
@@ -103,30 +206,28 @@ Command* LogicParser::createDeleteCommand() {
 	deleteCommand->setParsedStatus(LogicParser::isValidCommandWithIndexParameters());
 
 	if(deleteCommand->getParsedStatus()) {
-		int index = std::stoi(LogicParser::getParameters(_userInput));
-		deleteCommand->setTaskIndex(index);
+		LogicParser::addTaskIndex(deleteCommand);
 	}
 
 	return deleteCommand;
 }
 
-Command* LogicParser::createDisplayCommand() {
-	Command* displayCommand = new CommandDisplay;
-	displayCommand->setParsedStatus(LogicParser::isValidCommandWithNoParameters());
+Command* LogicParser::createDoneCommand() {
+	Command* doneCommand = new CommandDone;
+	doneCommand->setParsedStatus(LogicParser::isValidCommandWithIndexParameters());
 
-	return displayCommand;
-}
-
-Command* LogicParser::createEditCommand() {
-	Command* editCommand = new CommandEdit;
-	editCommand->setParsedStatus(LogicParser::isValidCommandWithIndexParameters());
-
-	if(editCommand->getParsedStatus()) {
-		int index = std::stoi(LogicParser::getParameters(_userInput));
-		editCommand->setTaskIndex(index);
+	if(doneCommand->getParsedStatus()) {
+		LogicParser::addTaskIndex(doneCommand);
 	}
 
-	return editCommand;
+	return doneCommand;
+}
+
+Command* LogicParser::createClearCommand() {
+	Command* clearCommand = new CommandClear;
+	clearCommand->setParsedStatus(LogicParser::isValidCommandWithNoParameters());
+
+	return clearCommand;
 }
 
 Command* LogicParser::createLoadCommand() {
@@ -143,18 +244,6 @@ Command* LogicParser::createRedoCommand() {
 	return redoCommand;
 }
 
-Command* LogicParser::createSearchCommand() {
-	Command* searchCommand = new CommandSearch;
-	searchCommand->setParsedStatus(LogicParser::isValidCommandWithStringParameters());
-
-	if(searchCommand->getParsedStatus()) {
-		std::string searchString = LogicParser::getParameters(_userInput);
-		searchCommand->setSearchKeyword(searchString);
-	}
-
-	return searchCommand;
-}
-
 Command* LogicParser::createUndoCommand() {
 	Command* undoCommand = new CommandUndo;
 	undoCommand->setParsedStatus(LogicParser::isValidCommandWithNoParameters());
@@ -169,10 +258,9 @@ bool LogicParser::isValidCommandWithStringParameters() {
 bool LogicParser::isValidCommandWithIndexParameters() {
 	if(LogicParser::isOneWord(_userInput)) {
 		return false;
-	}
-	else {
+	} else {
 		std::string parameters = LogicParser::getParameters(_userInput);
-		return std::all_of(parameters.begin(), parameters.end(), ::isdigit);
+		return std::all_of(parameters.begin(), parameters.end(), isdigit);
 	}
 }
 
@@ -196,7 +284,7 @@ std::string LogicParser::removeFirstWord(std::string text, const std::string& de
 }
 
 std::string LogicParser::transformToLowercase(std::string text) {
-	std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+	std::transform(text.begin(), text.end(), text.begin(), tolower);
 	return text;
 }
 
@@ -215,4 +303,16 @@ std::string LogicParser::trimLeft(const std::string& text, const std::string& de
 
 std::string LogicParser::trimRight(const std::string& text, const std::string& delimiters) {
 	return text.substr(0, text.find_last_not_of(delimiters) + 1);
+}
+
+std::vector<std::string> LogicParser::splitWordsIntoVector(std::string text) {
+	std::vector<std::string> taskParameters;
+	std::istringstream iss(text);
+	std::string currentWord;
+	
+	while(iss >> currentWord) {
+			taskParameters.push_back(currentWord);
+	}
+
+	return taskParameters;
 }
