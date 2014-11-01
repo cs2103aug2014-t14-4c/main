a1e1941a50765c04aa4659e9a13686e7bbd32697
Shu Chang <shuchang.91@gmail.com>
2014-11-01 22:15:32 +0800

    bleh

--- WhatToDo/LogicParserDetailsParser.cpp
+++ WhatToDo/LogicParserDetailsParser.cpp
@@ -156,8 +156,8 @@ void DetailsParser::parseDateFilter(Command* command) {
 		if(foundNoDate()) {
 			date positiveInfinity(pos_infin);
 			date negativeInfinity(neg_infin);
-			command->setStartDateFilter(positiveInfinity);
-			command->setEndDateFilter(negativeInfinity);
+			command->setStartDateFilter(negativeInfinity);
+			command->setEndDateFilter(positiveInfinity);
 		} else {
 			DatetimeParser datetime;
 			datetime.addFilterDate(command, _parameters);
